c209109e69937b2f534bc099d3c29c63f7bce746
yaoxin <yaoxin19950517@gmail.com>
2023-01-09 16:03:45 +0100

    add function that support encrypt io stream and decode the io stream

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,23 +1,75 @@
-use aes_gcm_example::{decrypt, encrypt};
+use aes_gcm_example::*;
 use anyhow::{bail, Context, Error};
+use std::io::prelude::*;
+use std::process::{Command, Stdio};
+use serde::{Serialize, Deserialize};
+use bincode::*;
 
-fn main() -> Result<(), Error> {
-    // 1. Secret Key
-    //  256-bits (32 bytes)
+static PANGRAM: &'static str =
+"the quick brown fox jumped over the lazy dog\n";
+
+static PANGRAM1: &'static str =
+"who are you docker-entrypoint.sh: /docker-entrypoint.d/ is not empty, will attempt to perform configuration\n";
+
+
+
+
+
+fn main() -> () {
+
     const KEY: &[u8; 32] = b"a very simple secret key to use!";
 
-    const PLAIN_TEXT: &[u8; 32] = b"a very simple message to encrypt";
+    const PLAIN_TEXT: &[u8] = b"a very simple message to encrypt";
+
+
+    const PLAIN_TEXT1: &[u8] = b"who are you docker-entrypoint.sh: /docker-entrypoint.d/ is not empty, will attempt to perform configurationt1111111111111111111111111111111111111111111111\n";
+
+    let mut payload1 = PayLoad::default();
+    payload1.counter = 1;
+    payload1.data = PLAIN_TEXT.to_vec();
+
+    let mut payload2 = PayLoad::default();
+    payload2.counter = 2;
+    payload2.data = PLAIN_TEXT1.to_vec();
+
+    let encodedIoFrame1: Vec<u8> = bincode::serialize(&payload1).unwrap();
+    let encodedIoFrame2: Vec<u8> = bincode::serialize(&payload2).unwrap();
+
+    let mut ioFrame1 = IoFrame::default();
+    let mut  ioFrame2 = IoFrame::default();
 
     // 2. Encrypt
-    let cipher_text = encrypt(KEY, PLAIN_TEXT.as_ref()).with_context(|| "encryption failure!")?;
+    (ioFrame1.pay_load, ioFrame1.nonce)= encrypt(KEY, encodedIoFrame1.as_ref()).with_context(|| "encryption failure!").unwrap();
 
-    // 3. Decrypt
-    let plain_text = decrypt(KEY, cipher_text.as_ref()).with_context(|| "decryption failure!")?;
+    (ioFrame2.pay_load, ioFrame2.nonce) = encrypt(KEY, encodedIoFrame2.as_ref()).with_context(|| "encryption failure!").unwrap();
 
-    // 4. Match!
-    if plain_text != PLAIN_TEXT {
-        bail!("decrypted text doesn't match!")
+
+    print!("ioFrame1 : {:?}\n", ioFrame1);
+    print!("ioFrame2 : {:?}\n", ioFrame2);
+
+
+    let encodedFrame1 = bincode::serialize(&ioFrame1).unwrap();
+    let encodedFrame2 = bincode::serialize(&ioFrame2).unwrap();
+
+    let mut encoded12 =  encodedFrame1.clone();
+    encoded12.append(encodedFrame2.clone().as_mut());
+
+    
+    let mut start= 0;
+    while start < encoded12.len() {
+        let frame1:IoFrame = bincode::deserialize(encoded12[start..].as_ref()).unwrap();
+        // let frame2:IoFrame = bincode::deserialize(encoded12[]).unwrap();
+        print!("frame111111111111 : {:?}\n", frame1);
+    
+    
+        let decrypted = decrypt(KEY, &frame1.pay_load, &frame1.nonce).unwrap();
+        let payload:PayLoad = bincode::deserialize(decrypted.as_ref()).unwrap();
+    
+    
+        print!("decrypted22222222222 {:?}, PLAIN_TEXT{:?}\n", payload, PLAIN_TEXT1.as_ref());
+
+        start = start + bincode::serialized_size(&frame1).unwrap() as usize;
+
     }
-
-    Ok(())
+    
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,106 @@
+use std::borrow::BorrowMut;
+
 use aes_gcm::aead::{Aead, NewAead};
 use aes_gcm::{Aes256Gcm, Key, Nonce};
 use anyhow::{bail, Error};
 use rand::{thread_rng, Rng};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct IoFrame {
+    pub nonce: Vec<u8>,
+    // length: usize,
+    // encrypted payload structure using aes-gcm
+    pub pay_load: Vec<u8>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default,Clone)]
+pub struct PayLoad {
+    pub counter: u128,
+    pub data: Vec<u8>,
+}
+
 
 /// Nonce: unique per message.
 /// 96-bits (12 bytes)
-const NONCE_LENGTH: usize = 12;
+pub const NONCE_LENGTH: usize = 12;
 
 fn random_bytes() -> [u8; NONCE_LENGTH] {
     thread_rng().gen::<[u8; NONCE_LENGTH]>()
 }
 
-pub fn encrypt(key: &[u8], plain_txt: &[u8]) -> Result<Vec<u8>, Error> {
+
+
+pub fn encrypt(key: &[u8], plain_txt: &[u8]) -> Result<(Vec<u8>, Vec<u8>), Error> {
     let cipher = Aes256Gcm::new(Key::from_slice(key));
     let nonce_rnd = random_bytes();
     let nonce = Nonce::from_slice(&nonce_rnd);
     let encrypt_msg = cipher.encrypt(nonce, plain_txt).map_err(Error::msg)?;
     let mut cipher_txt = Vec::new();
-    cipher_txt.extend_from_slice(&nonce_rnd);
+    // cipher_txt.extend_from_slice(&nonce_rnd);
     cipher_txt.extend(encrypt_msg);
-    Ok(cipher_txt)
+    Ok((cipher_txt, nonce_rnd.to_vec()))
 }
 
-pub fn decrypt(key: &[u8], cipher_txt: &[u8]) -> Result<Vec<u8>, Error> {
-    if cipher_txt.len() <= NONCE_LENGTH {
-        bail!("cipher text is invalid");
-    }
+pub fn decrypt(key: &[u8], cipher_txt: &[u8], nouce: &[u8]) -> Result<Vec<u8>, Error> {
+    // if cipher_txt.len() <= NONCE_LENGTH {
+    //     bail!("cipher text is invalid");
+    // }
     let cipher = Aes256Gcm::new(Key::from_slice(key));
-    let nonce_rnd = &cipher_txt[..NONCE_LENGTH];
-    let nonce = Nonce::from_slice(nonce_rnd);
+    // let nonce_rnd = &cipher_txt[..NONCE_LENGTH];
+    let nonce = Nonce::from_slice(nouce);
     let plain_txt = cipher
-        .decrypt(nonce, &cipher_txt[NONCE_LENGTH..])
+        .decrypt(nonce, &cipher_txt[..])
         .map_err(Error::msg)?;
     Ok(plain_txt)
+}
+
+
+pub fn prepareIoFrame(plainText :&[u8]) -> Result<Vec<u8>, Error> {
+
+    const KEY: &[u8; 32] = b"a very simple secret key to use!";
+
+    let mut payload = PayLoad::default();
+    payload.counter = 1;
+    payload.data = plainText.to_vec();
+
+    let encoded_payload: Vec<u8> = bincode::serialize(&payload).unwrap();
+
+    let mut io_frame = IoFrame::default();
+
+    (io_frame.pay_load, io_frame.nonce)= encrypt(KEY, encoded_payload.as_ref()).unwrap();
+
+    let encoded_frame = bincode::serialize(&io_frame).unwrap();
+
+    Ok(encoded_frame)
+}
+
+
+pub fn getPayloads(encoded_payload :&Vec<u8>) -> Result<Vec<PayLoad>, Error> {
+
+    const KEY: &[u8; 32] = b"a very simple secret key to use!";
+
+    let mut payloads = Vec::new();
+
+    let mut start= 0;
+    while start < encoded_payload.len() {
+        let frame1:IoFrame = bincode::deserialize(encoded_payload[start..].as_ref()).unwrap();
+        // let frame2:IoFrame = bincode::deserialize(encoded12[]).unwrap();
+        // print!("frame111111111111 : {:?}\n", frame1);
+    
+    
+        let decrypted = decrypt(KEY, &frame1.pay_load, &frame1.nonce).unwrap();
+        let payload:PayLoad = bincode::deserialize(decrypted.as_ref()).unwrap();
+
+        payloads.fill(payload);
+    
+    
+        // print!("decrypted22222222222 {:?}, PLAIN_TEXT{:?}\n", payload, PLAIN_TEXT1.as_ref());
+
+        start = start + bincode::serialized_size(&frame1).unwrap() as usize;
+
+    }
+
+    Ok(payloads)
+
 }
